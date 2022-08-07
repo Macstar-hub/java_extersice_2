de237c191d624e6a2c045d1ca07b85fcf786356f
macstar-hub <mehran.moradii@outlook.com>
2022-08-07 12:56:00 +0430

    palindromChecker v2

--- src/palindromChecker.java
+++ src/palindromChecker.java
@@ -10,10 +10,11 @@ public class palindromChecker {
 
     public static String palidromChecker(String palindromSample) {
         System.out.println("text is:" + palindromSample);
-        int lastCharacter = palindromSample.length() - 1;
-        System.out.println("First character is: " + palindromSample.charAt(0));
-        System.out.println("Last character is: " + palindromSample.charAt(lastCharacter));
-        if (palindromSample.charAt(lastCharacter) == palindromSample.charAt(0)) {
+        String palindromSampleLowercase = palindromSample.toLowerCase();
+        int lastCharacter = palindromSampleLowercase.length() - 1;
+        System.out.println("First character is: " + palindromSampleLowercase.charAt(0));
+        System.out.println("Last character is: " + palindromSampleLowercase.charAt(lastCharacter));
+        if (palindromSampleLowercase.charAt(lastCharacter) == palindromSampleLowercase.charAt(0)) {
             System.out.println("Result is true");
         } else {
             System.out.println("Result is false");
