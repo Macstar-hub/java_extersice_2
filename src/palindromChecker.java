31945cfa2a57b1038c6c83892a6d6c5739df430c
macstar-hub <mehran.moradii@outlook.com>
2022-08-07 13:15:25 +0430

    palindromChecker v2

--- src/palindromChecker.java
+++ src/palindromChecker.java
@@ -2,18 +2,28 @@ import java.util.Scanner;
 
 public class palindromChecker {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter sample strings: ");
-        String palindromSample = input.nextLine();
-        System.out.println(palidromChecker(palindromSample));
+        outer:
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter sample strings: ");
+            String palindromSample = input.nextLine();
+            System.out.println(palidromChecker(palindromSample));
+            System.out.println("\t *************** For exit press 0 and for continue press 1 : **********************");
+            int exitStatus = input.nextInt();
+            switch (exitStatus) {
+                case 0:
+                    break outer;
+                case 1:
+                    continue ;
+            }
+        }
     }
-
     public static String palidromChecker(String palindromSample) {
-        System.out.println("text is:" + palindromSample);
+        //System.out.println("text is:" + palindromSample);
         String palindromSampleLowercase = palindromSample.toLowerCase();
         int lastCharacter = palindromSampleLowercase.length() - 1;
-        System.out.println("First character is: " + palindromSampleLowercase.charAt(0));
-        System.out.println("Last character is: " + palindromSampleLowercase.charAt(lastCharacter));
+        //System.out.println("First character is: " + palindromSampleLowercase.charAt(0));
+        //System.out.println("Last character is: " + palindromSampleLowercase.charAt(lastCharacter));
         if (palindromSampleLowercase.charAt(lastCharacter) == palindromSampleLowercase.charAt(0)) {
             System.out.println("Result is true");
         } else {
